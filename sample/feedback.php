c08e83c981ededb63c205a3ae920aa3cda2469a4
Arjun Raj <arjunraj.pala@gmail.com>
2021-06-21 13:29:36 +0530

    Add files via upload
    feedback system and volunteer registration file

---
+++ sample/feedback.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+   <!-- Basic -->
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <!-- Mobile Metas -->
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+   <!-- Site Metas -->
+   <title>Life Line</title>
+   <meta name="keywords" content="">
+   <meta name="description" content="">
+   <meta name="author" content="">
+   <!-- Site Icons -->
+   <link rel="shortcut icon" href="images/fevicon.ico.png" type="image/x-icon" />
+   <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+   <!-- Site CSS -->
+   <link rel="stylesheet" href="style.css">
+   <!-- Colors CSS -->
+   <link rel="stylesheet" href="css/colors.css">
+   <!-- ALL VERSION CSS -->
+   <link rel="stylesheet" href="css/versions.css">
+   <!-- Responsive CSS -->
+   <link rel="stylesheet" href="css/responsive.css">
+   <!-- Custom CSS -->
+   <link rel="stylesheet" href="css/custom.css">
+   <!-- Modernizer for Portfolio -->
+   <script src="js/modernizer.js"></script>
+   <!-- [if lt IE 9] -->
+   </head>
+   <body class="clinic_version">
+      <!-- LOADER -->
+      <div id="preloader">
+         <img class="preloader" src="images/loaders/heart-loading2.gif" alt="">
+      </div>
+      <!-- END LOADER -->
+      <header>
+  
+      <div class="header-top wow fadeIn">
+            <div class="container">
+               <a class="navbar-brand" href="index.php"><img src="images/logo1.png" alt="image"></a>
+               <div class="right-header">
+                  
+               </div>
+            </div>
+         </div>
+         <div class="header-bottom wow fadeIn">
+            <div class="container">
+               <nav class="main-menu">
+                  <div class="navbar-header">
+                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i class="fa fa-bars" aria-hidden="true"></i></button>
+                  </div>
+                
+				  
+                  <div id="navbar" class="navbar-collapse collapse">
+                     <ul class="nav navbar-nav">
+                        <li><a class="active" href="index.php">Return to Home</a></li>
+                    
+                     </ul>
+                  </div>
+               <div class=col-4>
+               </div>
+               </nav>
+              
+            </div>
+         </div>
+
+         
+      
+   
+      <div id="service" class="services wow fadeIn">
+      <div class="col-lg-16 col-md-15 col-sm-19 col-xs-15">
+      
+                  <div class="appointment-form">
+                     <h3><span></span>Provide your Feedback on LifeLine</h3>
+                     <div class="form">
+                        <form method="post" action="sample/patient.php">
+                           <fieldset>
+                              
+                              
+                             <div class="container">
+                          <form action="action_page.php">
+                             <div>
+
+                          <label for="fname">First Name</label>
+                           <input type="text" id="fname" name="firstname" placeholder="Your name..">
+</div>
+                               <div>
+                          <label for="lname">Last Name</label>
+                             <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+                            </div>
+                             <div>
+                          <label for="country">Country &nbsp &nbsp &nbsp</label>
+                            <select id="country"name="country">
+							&nbsp
+							
+                              <option value="IND">India</option>
+                                 <option value="CAN">Canada</option>
+                                    <option value="usa">China</option>
+									<option value="usa">Russia</option>
+									<option value="usa">Japan</option>
+									<option value="usa">UK</option>
+									<option value="usa">Amsterdam</option>
+									<option value="usa">Others
+									</option>
+                            </select>
+                                 </div>
+                           <div>
+                          <label for="subject">Feedback</label>
+                            <textarea id="Feedback" name="Feedback" placeholder="What do you think ?..." style="height:200px"></textarea>
+                           </div>
+                           
+
+                            </form>
+                             </div>
+                              
+                              
+                              <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                 <div class="row">
+                                    <div class="form-group">
+                                       <div class="center"><button type="submit">Submit</button></div>
+									   
+                                    </div>
+                                 </div>
+                              </div>
+      
+                           </fieldset>
+                        </form>
+                     </div>
+                  </div>
+               </div>
+
+              
+                  
+               
+
+
+
+
+
+
+
+
+
+
+
+
+    
+	  
+	
+      <!-- end copyrights -->
+      <a href="#home" data-scroll class="dmtop global-radius"><i class="fa fa-angle-up"></i></a>
+      <!-- all js files -->
+      <script src="js/all.js"></script>
+      <!-- all plugins -->
+      <script src="js/custom.js"></script>
+      <!-- map -->
+     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCNUPWkb4Cjd7Wxo-T4uoUldFjoiUA1fJc&callback=myMap"></script>
+   </body>
+</html>
