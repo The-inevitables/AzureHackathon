cb0fbc63e62f35bd26be288d802b41c7cdd6df05
Arjun Raj <arjunraj.pala@gmail.com>
2021-06-21 11:28:50 +0530

    Update patient.php

--- sample/patient.php
+++ sample/patient.php
@@ -434,7 +434,7 @@
                         </div>
                         <div class="col-md-6">
                             <div class="text-md-right footer-links d-none d-md-block">
-                                <span> 2021 Team Inevitables  </span>
+                                <span>© 2021 LifeLine. Developed by Team INEVITABLES. All Rights Reserved.   </span>
                                 <a href="javascript: void(0);">About</a>
                                 <a href="javascript: void(0);">Support</a>
                                 <a href="javascript: void(0);">Contact Us</a>
